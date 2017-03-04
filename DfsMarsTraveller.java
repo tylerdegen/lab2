e0ef6c60f9b8a9b0cfb7daa8acde4d983cbc1ede
Tyler Degen <degen.19@osu.edu>
2017-03-04 17:59:29 -0500

    Implemented cost function

--- DfsMarsTraveller.java
+++ DfsMarsTraveller.java
@@ -15,6 +15,14 @@ public class DfsMarsTraveller {
 		has3 = false;
 		M = new MarsMap(datafile);
 	}
+	
+	public float cost(MarsPlace m1, MarsPlace m2){
+		float distance = M.getDistance(m1, m2);
+		float sum = has1 ? 1 : 0;
+		sum += has2 ? 1 : 0;
+		sum += has3 ? 1 : 0;
+		return distance + 100 * (3 - sum);
+	}
 
 	//test implementation of Traveller
 	public static void main(String args[]) {
@@ -25,15 +33,16 @@ public class DfsMarsTraveller {
 		
 		DfsMarsTraveller dmt = new DfsMarsTraveller("hw2-data1.txt");
 		MarsPlace startPlace = dmt.M.getPlace(startPoint);
-		
-		
+		float test = dmt.cost(startPlace, dmt.M.getPlace("B"));
 		
 		//if (startFlag != "-s" || startPoint == null ||  readFile ==""){
 			//System.out.print("ERROR");
 			//return;
 		//}
 		
+		
+		
 		System.out.println("it works!");
-		System.out.println(startFlag + startPlace.name() + readFile);
+		System.out.println(startFlag + startPlace.name() + readFile + test);
 	}
 }
